97778664393c31ccb4230d63366563689eea4430
deviant <mb.deviant@gmail.com>
2022-11-28 17:31:11 +0300

    add function to allow players place signs

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+const Player = (sign) => {
+    this.sign = sign;
 
+    const getSign = () => {
+        return sign;
+    };
+    return { getSign };
+}
 
 
 const gameBoard = (() => {
-    const board = ['X', '', 'X', '', 'O', '', 'X', '', 'X'];
+    const board = ['', '', '', '', '', '', '', '', ''];
     const getIndex = index => {
         return board[index];
     }
@@ -10,30 +17,55 @@ const gameBoard = (() => {
         board[index] = sign;
     }
     const reset = () => {
-        for (let i = 0; i < cells.length; i++) {
+        for (let i = 0; i < board.length; i++) {
             board[i] = '';
             console.log('reached');
         }
     }
-    return { getIndex, reset, setSign }
+    return { getIndex, reset, setSign, board }
 })();
 
 
 
 const displayControl = (() => {
-    const cells = document.querySelectorAll('[data-cell]');
-    cells.forEach(element => {
-        addEventListener("click", () => {
-            console.log('element');
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        addEventListener("click", (e) => {
+            gameBoard.setSign(parseInt(e.target.dataset.index), gameplay.currentSign());
+            gameplay.checkSign();
+            updateGameboard();
         })
     });
+
     const updateGameboard = () => {
         for (let i = 0; i < cells.length; i++) {
             cells[i].textContent = gameBoard.getIndex(i);
-            console.log(cells[i])
+            // console.log(cells[i])
         }
     };
     return { updateGameboard }
 })();
 
+const gameplay = (() => {
+    const playerX = Player('X');
+    const playerO = Player('O')
+    let activePlayer = playerX;
+
+    const currentSign = (sign) => {
+        sign = activePlayer.getSign();
+        return sign
+    }
+
+
+    const checkSign = () => {
+        activePlayer == playerX ? activePlayer = playerO : activePlayer = playerX;
+       
+    }
+
+    return { checkSign, currentSign };
+})();
+gameplay.currentSign();
+
 displayControl.updateGameboard();
+gameBoard.reset();
+
